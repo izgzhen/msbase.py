c21b2090eb501ef56c6c077b8efc8add8ba27992
Zhen Zhang <izgzhen@gmail.com>
2019-03-18 11:04:29 -0700

    add lab

--- msbase/utils.py
+++ msbase/utils.py
@@ -1,11 +1,21 @@
 import json
+import jsonlines
 import time
 
-def load_json(p: str):
-    return json.load(open(p, "r"))
+def load_json(path: str):
+    return json.load(open(path, "r"))
 
 def write_pretty_json(stuff, path: str):
-    open(path, 'w').write(json.dumps(stuff, indent=4, sort_keys=True))
+    with open(path, 'w') as f:
+        f.write(json.dumps(stuff, indent=4, sort_keys=True))
+
+def append_pretty_json(stuff, path: str):
+    with jsonlines.open(path, mode='a') as f:
+        f.write(stuff)
 
 def datetime_str():
     return time.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+def load_jsonl(path: str):
+    with jsonlines.open(path) as reader:
+        return [obj for obj in reader]
--- test.py
+++ test.py
@@ -20,3 +20,26 @@ def task(i):
     return i + 1
 
 print(multiprocess(task, [1, 2, 3], n = 2))
+
+from msbase.lab import Step, AbstractLab, to_matrix
+
+config1 = { "A": ["A1", "A2" ], "B": ["B1", "B2"] }
+print(to_matrix(config1))
+
+step1 = Step("mysleep1", ["./mysleep", "1"],
+             configurations={ "A": ["A1", "A2" ], "B": ["B1", "B2"] })
+step2 = Step("mysleep2", ["./mysleep", "2"],
+             configurations={ "A": ["A3", "A4" ], "B": ["B3", "B4"] })
+
+class MyLab(AbstractLab):
+    def digest_output(self, name: str, output):
+        return {}
+
+lab = MyLab("mylab", [step1, step2])
+lab.run()
+
+import os
+f = [f for f in os.listdir(".") if f.endswith(".log")][0]
+from msbase.utils import load_jsonl
+assert len(load_jsonl(f)) == 8
+os.remove(f)
