bac2bb8160f11c1b256b2c37a35db7df6ef3ac92
Zhen Zhang <izgzhen@gmail.com>
2019-03-18 11:04:50 -0700

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="msbase",
-    version="0.0.4",
+    version="0.0.5",
     author="Zhen Zhang",
     # author_email="",
     description="Base Library for Python for MySelf",
