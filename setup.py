43620557aa5a4cbdbc884bdd086d1328ac12d857
Zhen Zhang <git@zhen-zhang.com>
2020-02-24 15:15:18 -0800

    Upgrade

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="msbase",
-    version="0.2.3",
+    version="0.2.4",
     author="Zhen Zhang",
     author_email="pip@zhen-zhang.com",
     description="Base Library for Python for MySelf",
